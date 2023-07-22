5ada14dcadd4cfbbbe2bbf496d7b9e2fd345bdf0
jchtech <139909582+jchtech@users.noreply.github.com>
2023-07-22 19:54:44 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 window.isAuthenticated = false;   // variable global que nos dice si se ha podido autenticar o no
 window.identity = {};             // diccionario con los datos recuperados, ya descodificados desde el token
 window.token = '';                // token recuperado
+var url = "https://script.google.com/macros/s/AKfycbw_MLUJXZe99Pk4YqNcfv6Ks1l9aYFBkPLKAbgUlXBOy_ImosqyuLMg5DAROr-wCpw1/exec?";
 
 // funciones para decodificar el token devuelto por Google
 // Source: https://stackoverflow.com/a/47574303/4064162 Answer by user "Rafael Quintela"
@@ -32,7 +33,7 @@ window.onload = function () {
   google.accounts.id.initialize({
       client_id: "1009336245126-7a1g0qdglr9l7svpusr60h77bps9h6pr.apps.googleusercontent.com",
       callback: handleCredentialResponse,
-      auto_select: true,
+      auto_select: false
   });
   google.accounts.id.renderButton(
       document.getElementById("buttonDiv"),
@@ -43,16 +44,21 @@ window.onload = function () {
 
 // si está logado, muestra la info de cabecera, nombre y foto y oculta botón de one-tap
 function showAuthInfo() {
+
   if (window.isAuthenticated) { 
-    console.log("llega a IS AUTHENTICATED");
-    //document.getElementById("cuerpo").style.removeProperty('display');
-    //document.getElementById("authenticated").style.setProperty('display', 'none');
-    document.getElementById("welcome").innerHTML = '<img src="' + window.identity.picture + '" style="padding: 0 2rem 0 2rem; border-radius: 50%;">';
-    console.log("datos:" + window.identity.name + ' ' + window.identity.email);
+
+    console.log("AUTHENTICATED IN AFAC");
+    console.log("datos:" + window.identity.name + ' ' + window.identity.email + ' ' + window.identity.picture);
+    document.getElementById("cuerpo").style.removeProperty('display');
+    document.getElementById("login").style.setProperty('display', 'none'); 
+    url = url + "email=" + window.identity.email; 
+    document.getElementById("gas_afac").style.setProperty('src', url); 
+     
   } else {
-    console.log("PUES LLEGA AQUI, NOT AUTHENTICATED");
-    //document.getElementById("authenticated").style.setProperty('display', 'none');
+
+    console.log("NOT AUTHENTICATED");
+    //document.getElementById("login").style.setProperty('display', 'none');
     //document.getElementById("cuerpo").style.setProperty('display', 'none');
-    document.getElementById("welcome").innerText = 'NOT AUTHENTICATED';
+    document.getElementById("welcome").innerText = "Si us plau, utilitzeu un compte de google per fer login a l'aplicació";
   }
 }
